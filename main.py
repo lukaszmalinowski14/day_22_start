6d6aad6c54a07f31ea4be1d45874c4fc93356fa1
lukaszmalinowski14 <lukaszmalinowski14@gmail.com>
2023-08-01 11:19:42 +0200

    "Pong_1_2"

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import Turtle, Screen
 
 # 1. Create the screen
 # 2. Create and move a paddle
@@ -7,3 +8,15 @@
 # 6. Detect collision with paddle
 # 7. Detect when paddle misses
 # 8. Keep score
+
+
+# Ad1
+
+scren = Screen()
+scren.screensize(800, 600)
+scren.bgcolor("black")
+scren.title("PONG")
+
+
+# exit when click
+scren.exitonclick()
